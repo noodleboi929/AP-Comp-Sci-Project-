32b13aa4a78b6ebf1cd75081a32eb97d4bc0b692
noodleboi929 <akashmacha73@gmail.com>
2021-06-14 19:22:07 -0400

    push

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,41 +7,37 @@ const AboutPage = () => {
   return (
     <Layout>
       <h1 className={aboutStyle.heads}>About:</h1>
-      <div className={aboutStyle.flexer}>
+      <div className={aboutStyle.flexing}>
+        <div className={aboutStyle.flexer}>
+          <p>
+            My Name is Akash and I am currently a sophmore at Ridge High School.
+            However I am not like a normal sophmore. Even though most of my
+            peers are already 16, I wont turn 16 until next year. I personally
+            think that I am smart, funny, and charismatic, but I guess thats for
+            you to decide.
+          </p>
+        </div>
         <a
           href="http://bernardsboe-ridgehigh.ss5.sharpschool.com/"
           target="__blank"
         >
           <img src="ridge.svg" />
         </a>
-        <p>
-          My Name is Akash and I am currently a sophmore at Ridge High School.
-        </p>
-        <p>
-          However I am not like a normal sophmore. Even though most of my peers
-          are already 16, I wont turn 16 until next year.
-        </p>
-        <p>
-          I personally think that I am smart, funny, and charismatic, but I
-          guess thats for you to decide.
-        </p>
+        <div className={aboutStyle.flexer}>
+          <p>
+            I am really excited for the summer this year becuase I finally got
+            my first internship. I am really interest in computer science and
+            bioinformatics and plan on studying it in college. The internship is
+            at MSK and I get to shadow a bioinformatics professional !
+          </p>
+        </div>
         <a href="https://www.mskcc.org/" target="__blank">
           <img src="msk.svg" />
         </a>
-        <p>
-          I am really excited for the summer this year becuase I finally got my
-          first internship.{" "}
-        </p>
-        <p>
-          I am really interest in computer science and bioinformatics and plan
-          on studying it in college.
-        </p>
-        <p>
-          The internship is at MSK and I get to shadow a bioinformatics
-          professional !
-        </p>
+        <div className={aboutStyle.flexer}>
+          <p>Enjoy Your Summer !!!</p>
+        </div>
         <img src="smile.svg" />
-        <p>Enjoy Your Summer !!!</p>
       </div>
     </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,29 +11,21 @@ const IndexPage = () => {
       </div>
       <div className={indexStyles.marginStyle}>
         <h1 className={indexStyles.heads}>Hello:</h1>
-        <div className={indexStyles.flexer}>
-          <h2>This is my first Gatsby Site, so I hope you enjoy!</h2>
-          <p>
-            Gatsby is a static site generator that is React-based and is powered
-            by Graphql.
-          </p>
-          <p>
-            The variety of plugins and vast Community allows you to do amazing
-            things in Gatsby that you couldn't do in just HTML.
-          </p>
-          <p>
-            I just started getting interested in HTML and web development again,
-            so this is a big milestone for me.
-          </p>
-          <p>
-            The crown Jewel of this website is the blog page. In simple terms, I
-            am able to post content form a third party without having to edit my
-            base code.
-          </p>
-          <p>
-            I hope you enjoy the website and feel free to contact me regarding
-            anything
-          </p>
+        <div className={indexStyles.flexing}>
+          <div className={indexStyles.flexer}>
+            <h2>This is my first Gatsby Site, so I hope you enjoy!</h2>
+            <p>
+              Gatsby is a static site generator that is React-based and is
+              powered by Graphql. The variety of plugins and vast Community
+              allows you to do amazing things in Gatsby that you couldn't do in
+              just HTML. I just started getting interested in HTML and web
+              development again, so this is a big milestone for me. The crown
+              Jewel of this website is the blog page. In simple terms, I am able
+              to post content form a third party without having to edit my base
+              code. I hope you enjoy the website and feel free to contact me
+              regarding anything
+            </p>
+          </div>
           <a href="https://www.gatsbyjs.com/" target="__blank">
             <img src="gatsby.svg" />
           </a>
